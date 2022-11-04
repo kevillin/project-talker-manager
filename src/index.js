d57998b1e64dc84c3b8f5a6194def074e74488cd
Kevillin Santos <kevillin_santos@hotmail.com>
2022-11-04 20:38:34 -0300

    requisito 5

--- src/index.js
+++ src/index.js
@@ -2,7 +2,10 @@ const express = require('express');
 const path = require('path');
 const fs = require('fs').promises;
 const bodyParser = require('body-parser');
-const { generateToken, validateFields } = require('./middlewares');
+const { generateToken, validateFields, validateAutorizationAndName,
+  validateAge,
+  validateTalk,
+  validateTalk2 } = require('./middlewares');
 
 const talkersArchive = path.resolve(__dirname, './talker.json');
 
@@ -41,12 +44,29 @@ app.get('/talker/:id', async (req, res) => {
   return res.status(HTTP_OK_STATUS).json(filter);
 });
 
-app.post('/login', validateFields, async (req, res) => {
-  const result = await fs.readFile(talkersArchive, 'utf-8');
+app.post('/login', validateFields, async (_req, res) => {
+  // const result = await fs.readFile(talkersArchive, 'utf-8');
   const tokenGen = generateToken();
-  const newPerson = { ...req.body };
-  const newPersonFile = [...JSON.parse(result), newPerson];
-  await fs.writeFile(talkersArchive, JSON.stringify(newPersonFile));
+  // const newPerson = { ...req.body };
+  // const newPersonFile = [...JSON.parse(result), newPerson];
+  // await fs.writeFile(talkersArchive, JSON.stringify(newPersonFile));
 
   res.status(HTTP_OK_STATUS).json({ token: tokenGen });
+});
+
+app.post('/talker',
+  validateAutorizationAndName,
+  validateAge,
+  validateTalk,
+  validateTalk2,
+  async (req, res) => {
+    const person = req.body;
+    const result = await fs.readFile(talkersArchive, 'utf-8');
+    const parseTalkers = JSON.parse(result);
+    const nextId = parseTalkers[parseTalkers.length - 1].id + 1;
+    const newPerson = { id: nextId, ...person };
+    const newPersonFile = [...parseTalkers, newPerson];
+    await fs.writeFile(talkersArchive, JSON.stringify(newPersonFile));
+
+    return res.status(201).json(newPerson);
 });
--- src/middlewares.js
+++ src/middlewares.js
@@ -20,4 +20,64 @@ const validateFields = (req, res, next) => {
   return next();
 };
 
-module.exports = { generateToken, validateFields };
+const validateAutorizationAndName = (req, res, next) => {
+  const { name } = req.body;
+  const { authorization } = req.headers;
+
+  if (!authorization) return res.status(401).json({ message: 'Token não encontrado' });
+  if (authorization.length < 16) return res.status(401).json({ message: 'Token inválido' });
+  if (!name) return res.status(400).json({ message: 'O campo "name" é obrigatório' });
+  if (name.length < 3) {
+    return res.status(400).json({ message: 'O "name" deve ter pelo menos 3 caracteres' });
+  }
+
+  return next();
+};
+
+const validateAge = (req, res, next) => {
+  const { age } = req.body;
+
+  if (!age) return res.status(400).json({ message: 'O campo "age" é obrigatório' });
+  if (Number(age) < 18) {
+    return res.status(400).json({ message: 'A pessoa palestrante deve ser maior de idade' });
+  }
+
+  return next();
+};
+
+const validateTalk = (req, res, next) => {
+  const regexDate = /(\d{2})\/?(\d{2})?\/(\d{4})/;
+  const { talk } = req.body;
+  if (!talk) {
+    return res.status(400).json({ message: 'O campo "talk" é obrigatório' });
+  }
+  if (!talk.watchedAt) {
+    return res.status(400).json({ message: 'O campo "watchedAt" é obrigatório' });
+  }
+  if (!regexDate.test(talk.watchedAt)) {
+    return res.status(400).json({ message: 'O campo "watchedAt" deve ter o formato "dd/mm/aaaa"' });
+  }
+  return next();
+};
+
+const validateTalk2 = (req, res, next) => {
+  const { talk } = req.body;
+  const er = /^-?[0-9]+$/;
+
+  if (!talk.rate) {
+    return res.status(400).json({ message: 'O campo "rate" é obrigatório' });
+  }
+  if (Number(talk.rate) < 1 || Number(talk.rate) > 5 || !er.test(Number(talk.rate))) {
+    return res.status(400).json({ message: 'O campo "rate" deve ser um inteiro de 1 à 5' });
+  }
+  return next();
+};
+
+module.exports = {
+  generateToken,
+  validateFields,
+  validateAutorizationAndName,
+  validateAge,
+  validateTalk,
+  validateTalk2,
+};
