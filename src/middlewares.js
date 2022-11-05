790e02a1328636874f7cabb2b83b2d8f34b302db
Kevillin Santos <kevillin_santos@hotmail.com>
2022-11-05 11:16:16 -0300

    requisito 6

--- src/middlewares.js
+++ src/middlewares.js
@@ -70,13 +70,12 @@ const validateTalk = (req, res, next) => {
 };
 
 const validateTalk2 = (req, res, next) => {
-  const { talk } = req.body;
-  const er = /^-?[0-9]+$/;
+  const { rate } = req.body.talk;
+  // const er = /^-?[0-9]+$/;
 
-  if (!talk.rate) {
+  if (!('rate' in req.body.talk)) {
     return res.status(400).json({ message: 'O campo "rate" é obrigatório' });
-  }
-  if (Number(talk.rate) < 1 || Number(talk.rate) > 5 || !er.test(Number(talk.rate))) {
+  } if (rate < 1 || rate > 5 || !Number.isInteger(rate)) {
     return res.status(400).json({ message: 'O campo "rate" deve ser um inteiro de 1 à 5' });
   }
   return next();
